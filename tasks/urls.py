29085963b0331794864f7992ea79a6a38b33c51f
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-10-30 15:00:23 +0000

    creating a layout page

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks" # We give a name the the url path because of name colision, meaning that if we have two path with the name "index", they will colide. and if we give a name the the app  this wount happend.
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
